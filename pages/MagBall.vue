c076f504595e7ab7bd74007c4c1e620513452032
Xavier Bell <xavier.l.bell@gmail.com>
2022-08-01 15:51:59 -0400

    Added nuxt workspace

---
+++ pages/MagBall.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      this is the first thing
+      <nuxt-logo/>
+  </div>
+</template>
+
+<script>
+import NuxtLogo from '../components/NuxtLogo.vue'
+export default {
+  components: { NuxtLogo },
+  name: 'IndexPage'
+}
+</script>
